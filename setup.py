856a2231e4253a53ea3af9be4839196c8b01f216
ViCky <vicky.chawda@gmail.com>
2017-05-21 17:07:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'ComplexHTTPServer',
   packages = ['ComplexHTTPServer'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = ' A Multithreaded Python SimpleHTTPServer',
   author = 'ViCky Chawda',
   author_email = 'vicky.chawda@gmail.com',
   url = 'https://github.com/vickysam/ComplexHTTPServer',
-  download_url = 'https://github.com/vickysam/ComplexHTTPServer/tarball/0.1',
+  download_url = 'https://github.com/vickysam/ComplexHTTPServer/tarball/0.2',
   keywords = ['http', 'simpehttpserver', 'multithreading'],
   classifiers = [],
 )
